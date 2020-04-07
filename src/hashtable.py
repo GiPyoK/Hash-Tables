2dfd16fa6925af15f54237db2c418043d468a6f2
Gi Pyo Kim <gipyok@gmail.com>
2020-04-07 15:37:54 -0700

    resize for insert works

--- src/hashtable.py
+++ src/hashtable.py
@@ -54,38 +54,33 @@ class HashTable:
         # Part 2: Change this so that hash collisions are handled with Linked List Chaining.
         '''
         # Check if HashTable has enough capacity
-        # if self.count >= self.capacity:
-        #     self.resize()
+        if self.count/self.capacity > 0.7:
+            self.resize()
 
         index = self._hash_mod(key)
-
-        # Part 1: Error handling
-        # if self.storage[index]:
-        #     print(f"Index {index} already exists in the hash table.\nUnable to insert <{key} : {value}>")
-        #     return
         
         # Part 2: Linked List Chaining
         if self.storage[index]:
             # Point to tail
             node = self.storage[index]
 
-            while node != None:
+            while node is not None:
                 # if the key already exists, overwirte
-                if node.key == self._hash(key):
+                if node.key == key:
                     node.value = value
                     return
                 
-                if node.next == None:
+                if node.next is None:
                     break
                 node = node.next
             
             # Insert the key value pair at the tail
-            node.next = LinkedPair(self._hash(key), value)
+            node.next = LinkedPair(key, value)
             self.count += 1
             return
             
         # insert key value pair
-        self.storage[index] = LinkedPair(self._hash(key), value)
+        self.storage[index] = LinkedPair(key, value)
         self.count += 1
 
 
@@ -100,7 +95,7 @@ class HashTable:
         index = self._hash_mod(key)
 
         # Check if the key exists
-        if self.storage[index] == None:
+        if self.storage[index] is None:
             print(f"Key: {key} does not exists.")
             return
         
@@ -110,15 +105,19 @@ class HashTable:
         prev = head
 
         # if the first linked list has the key
-        if prev.key == self._hash(key):
+        if prev.key == key:
             self.storage[index] = node
             self.count -= 1
+            # if self.count/self.capacity < 0.2:
+            #     self.resize()
             return
 
-        while node != None:
-            if node.key == self._hash(key):
+        while node is not None:
+            if node.key == key:
                 prev.next = node.next
                 self.count -= 1
+                # if self.count/self.capacity < 0.2:
+                #     self.resize()
                 return
             node = node.next
             prev = prev.next
@@ -138,8 +137,8 @@ class HashTable:
 
         # Loop through the linked list to find the key
         node = self.storage[index]
-        while node != None:
-            if node.key == self._hash(key):
+        while node is not None:
+            if node.key == key:
                 return node.value
             node = node.next
         
@@ -151,69 +150,95 @@ class HashTable:
         Doubles the capacity of the hash table and
         rehash all key/value pairs.
         '''
-        # Double the capacity
-        self.capacity *= 2
+        # resize the capacity
+        if self.count/self.capacity > 0.7:
+            self.capacity *= 2
+        elif self.count/self.capacity < 0.2:
+            self.capacity /= 2
 
         # Allocate new memory
         new_storage = [None] * self.capacity
 
         # Copy the old data into the new storage
-        for i in range(self.count):
-            new_storage[i] = self.storage[i]
+        for i in range(len(self.storage)):
+            if self.storage[i] is None:
+                continue
+
+            node = self.storage[i]
+
+            # traverse through self.storage linked list
+            while node is not None:
+                index = self._hash_mod(node.key)
+
+                # if new_storage[index] is empty, store the key value pair
+                if new_storage[index] is None:
+                    new_storage[index] = node
+                    node = node.next                # move to next node
+                    new_storage[index].next = None  # remove next connection
+                else:
+                    new_node = new_storage[index]
+                    # go to the tail of the new_node
+                    while new_node.next is not None:
+                        new_node = new_node.next
+                    new_node.next = node            # insert the node at the tail
+                    node = node.next                # move to next node
+                    new_node.next.next = None       # remove next connection
+
+        # replace the storage
         self.storage = new_storage
 
 
 
 
-if __name__ == "__main__":
-    ht = HashTable(2)
+# if __name__ == "__main__":
+#     ht = HashTable(2)
 
-    ht.insert("line_1", "Tiny hash table")
-    ht.insert("line_2", "Filled beyond capacity")
-    ht.insert("line_3", "Linked list saves the day!")
+#     ht.insert("line_1", "Tiny hash table")
+#     ht.insert("line_2", "Filled beyond capacity")
+#     ht.insert("line_3", "Linked list saves the day!")
 
-    print("")
+#     print("")
 
-    # Test storing beyond capacity
-    print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
+#     # Test storing beyond capacity
+#     print(ht.retrieve("line_1"))
+#     print(ht.retrieve("line_2"))
+#     print(ht.retrieve("line_3"))
 
-    # Test resizing
-    old_capacity = len(ht.storage)
-    ht.resize()
-    new_capacity = len(ht.storage)
+#     # Test resizing
+#     old_capacity = len(ht.storage)
+#     ht.resize()
+#     new_capacity = len(ht.storage)
 
-    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+#     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
-    # Test if data intact after resizing
-    print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
+#     # Test if data intact after resizing
+#     print(ht.retrieve("line_1"))
+#     print(ht.retrieve("line_2"))
+#     print(ht.retrieve("line_3"))
 
-    print("")
+#     print("")
 
 
-# ht = HashTable(4)
-# ht.insert("key1", "1")
-# ht.insert("key2", "2")
-# ht.insert("key3", "3")
-# ht.insert("key4", "4")
+ht = HashTable(4)
+ht.insert("key1", "1")
+ht.insert("key2", "2")
+ht.insert("key3", "3")
+ht.insert("key4", "4")
 
-# print(ht.retrieve("key1"))
-# print(ht.retrieve("key2"))
-# print(ht.retrieve("key3"))
-# print(ht.retrieve("key4"))
+print(ht.retrieve("key1"))
+print(ht.retrieve("key2"))
+print(ht.retrieve("key3"))
+print(ht.retrieve("key4"))
 
-# ht.insert("key1", "11")
-# ht.insert("key2", "22")
-# ht.insert("key3", "33")
-# ht.insert("key4", "44")
+ht.insert("key1", "11")
+ht.insert("key2", "22")
+ht.insert("key3", "33")
+ht.insert("key4", "44")
 
-# print(ht.retrieve("key1"))
-# print(ht.retrieve("key2"))
-# print(ht.retrieve("key3"))
-# print(ht.retrieve("key4"))
+print(ht.retrieve("key1"))
+print(ht.retrieve("key2"))
+print(ht.retrieve("key3"))
+print(ht.retrieve("key4"))
 
-# ht.remove("key1")
-# print(ht.retrieve("key1"))
+ht.remove("key1")
+print(ht.retrieve("key1"))
